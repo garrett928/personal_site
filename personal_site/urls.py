66f24c16180926da6bd70827bd8f226e9d180c7f
garrett928 <garrett7621@gmail.com>
2020-07-17 00:03:09 -0500

    django rest template site w/ admin enabled

---
+++ personal_site/urls.py
@@ -0,0 +1,29 @@
+"""personal_site URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from xml.etree.ElementInclude import include
+
+from django.contrib import admin
+from django.urls import path, re_path
+
+import portfolio
+from portfolio import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path(r'^api-auth/', include('rest_framework.urls')), broke, idk why
+    path(r'', views.AllTech.as_view()),
+    re_path(r'(?P<pk>\d+)', views.TechView.as_view()),
+]
---
+++ portfolio/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class Tech(models.Model):
+    short_name = models.CharField(max_length=10)
+    name = models.CharField(max_length=200)
